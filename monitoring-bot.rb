eee31d71bda759b83cbab8c8e1247f6886f7fa58
Gianluca Venturini <gianluca.91@gmail.com>
2015-04-06 12:41:02 -0500

    updated to new Nutella API

--- monitoring-bot.rb
+++ monitoring-bot.rb
@@ -2,16 +2,20 @@ require 'nutella_lib'
 require 'json'
 
 
+# Parse command line arguments
+broker, app_id, run_id = nutella.parse_args ARGV
+# Extract the component_id
+component_id = nutella.extract_component_id
 # Initialize nutella
-nutella.init("crepe", "localhost", "monitoring-bot")
+nutella.init(broker, app_id, run_id, component_id)
 
 # Open the resources database
-$applications = nutella.persist.getJsonStore('db/applications.json')
+$applications = nutella.persist.get_json_object_store('applications.json')
 
 puts 'Monitoring bot initialization'
 
 # Add an alert on a specific application/instance/component
-nutella.net.subscribe('monitoring/alert/add', lambda do |message, component_id, resource_id|
+nutella.net.subscribe('monitoring/alert/add', lambda do |message, from|
   puts message
   application = message['application']
   instance = message['instance']
@@ -24,29 +28,27 @@ nutella.net.subscribe('monitoring/alert/add', lambda do |message, component_id, 
     puts "Subscribed to #{application} with mail #{mail}";
   end
 
-  $applications.transaction{
-    if application != nil && instance != nil && component != nil
-      if $applications[application]['instances'][instance]['components'][component]['alert'] == nil
-        $applications[application]['instances'][instance]['components'][component]['alert'] = []
-      end
-      $applications[application]['instances'][instance]['components'][component]['alert'].push(mail)
-    elsif application != nil && instance != nil
-      if $applications[application]['instances'][instance]['alert'] == nil
-        $applications[application]['instances'][instance]['alert'] = []
-      end
-      $applications[application]['instances'][instance]['alert'].push(mail)
-    elsif application != nil
-      if $applications[application]['alert'] == nil
-        $applications[application]['alert'] = []
-      end
-      $applications[application]['alert'].push(mail)
+  if application != nil && instance != nil && component != nil
+    if $applications[application]['instances'][instance]['components'][component]['alert'] == nil
+      $applications[application]['instances'][instance]['components'][component]['alert'] = []
     end
-  }
+    $applications[application]['instances'][instance]['components'][component]['alert'].push(mail)
+  elsif application != nil && instance != nil
+    if $applications[application]['instances'][instance]['alert'] == nil
+      $applications[application]['instances'][instance]['alert'] = []
+    end
+    $applications[application]['instances'][instance]['alert'].push(mail)
+  elsif application != nil
+    if $applications[application]['alert'] == nil
+      $applications[application]['alert'] = []
+    end
+    $applications[application]['alert'].push(mail)
+  end
 
 end)
 
 # Listen for published messages
-nutella.net.subscribe('#', lambda do |message, channel, component_id, resource_id|
+nutella.net.subscribe('#', lambda do |message, channel, from|
   puts message
   puts channel
   application = 'application1'
@@ -55,15 +57,14 @@ nutella.net.subscribe('#', lambda do |message, channel, component_id, resource_i
 
   create_if_not_present(application, instance, component)
 
-  $applications.transaction{
-    if !($applications[application]['instances'][instance]['components'][component]['publish'].include? channel)
-      $applications[application]['instances'][instance]['components'][component]['publish'].push(channel)
-    end
-  }
+  if !($applications[application]['instances'][instance]['components'][component]['publish'].include? channel)
+    $applications[application]['instances'][instance]['components'][component]['publish'].push(channel)
+  end
+
 end)
 
 # Request the list of alert for an application/instance/component
-nutella.net.handle_requests('monitoring/alert', lambda do |request, component_id, resource_id|
+nutella.net.handle_requests('monitoring/alert', lambda do |request, from|
   puts 'Sending alert list'
   application = request['application']
   instance = request['instance']
@@ -71,16 +72,15 @@ nutella.net.handle_requests('monitoring/alert', lambda do |request, component_id
 
   alert = nil
 
-  $applications.transaction {
-    puts $applications['application1']['alert']
-    if application != nil && instance != nil && component != nil
-      alert = $applications[application]['instances'][instance]['components'][component]['alert']
-    elsif application != nil && instance != nil
-      alert = $applications[application]['instances'][instance]['alert']
-    elsif application != nil
-      alert = $applications[application]['alert']
-    end
-  }
+  puts $applications['application1']['alert']
+  if application != nil && instance != nil && component != nil
+    alert = $applications[application]['instances'][instance]['components'][component]['alert']
+  elsif application != nil && instance != nil
+    alert = $applications[application]['instances'][instance]['alert']
+  elsif application != nil
+    alert = $applications[application]['alert']
+  end
+
 
   if alert == nil
     alert = []
@@ -91,30 +91,28 @@ end)
 
 # Create the application structure if it is not present
 def create_if_not_present(application, instance, component)
-  $applications.transaction {
-    if $applications[application] == nil
-      $applications[application] = {
-          :name => application,
-          :instances => {}
-      }
-    end
+  if $applications[application] == nil
+    $applications[application] = {
+        :name => application,
+        :instances => {}
+    }
+  end
 
-    if instance != nil && $applications[application]['instances'][instance] == nil
-      $applications[application]['instances'][instance] = {
-          :name => instance,
-          :components => {}
-      }
-    end
+  if instance != nil && $applications[application]['instances'][instance] == nil
+    $applications[application]['instances'][instance] = {
+        :name => instance,
+        :components => {}
+    }
+  end
 
-    if component != nil && $applications[application]['instances'][instance]['components'][component] == nil
-      $applications[application]['instances'][instance]['components'][component] = {
-          :publish => [],
-          :subscribe => [],
-          :request => [],
-          :handle_request => []
-      }
-    end
-  }
+  if component != nil && $applications[application]['instances'][instance]['components'][component] == nil
+    $applications[application]['instances'][instance]['components'][component] = {
+        :publish => [],
+        :subscribe => [],
+        :request => [],
+        :handle_request => []
+    }
+  end
 end
 
 puts 'Initialization completed'
